574b4c4924843cdefa7a3aa4d18c3bf7f55c7166
hbeck <harald.beck@gmx.at>
2012-07-14 19:18:10 +0200

    removed old test cases

--- test/ueberfoo/test/parsing.clj
+++
@@ -1,7 +0,0 @@
-(ns ueberfoo.test.parsing
-  (:use [ueberfoo.parsing])
-  (:use [clojure.test]))
-
-(deftest opt-map-generation
-  (is (= {:select [:text], :display "v"}
-         (parse-list-options []))))
